6a0884539703c11b28e91941c1d53efe3db2ce23
Jin Rong Jian <jrong8018@gmail.com>
2024-01-19 14:34:20 -0800

    develop a text files web app using MVC

---
+++ Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextFilesMVC;
+
+public class FileController : Controller
+{
+    private readonly ILogger<FileController> _logger;
+    private readonly IWebHostEnvironment _env;
+
+    public FileController(ILogger<FileController> logger, IWebHostEnvironment env)
+    {
+        _logger = logger;
+        _env = env;
+    }
+
+    public IActionResult Index()
+    {
+		var path = Path.Combine(_env.ContentRootPath, "TextFiles");
+		ViewBag.Files = Directory.GetFiles(path);
+        // _logger.LogInformation(path);		
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TextFilesMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
